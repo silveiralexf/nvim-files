5ecaad5f410d145e243c9db63db5a93a49c284bf
Felipe Silveira <felipe.silveira@farfetch.com>
2023-09-07 19:35:44 +0100

    chore: add go, groovy and yaml settings for null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -10,21 +10,25 @@ return {
     null_ls.setup {
       sources = {
         null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.checkmake,
         null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.npm_groovy_lint,
         null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.formatting.beautysh,
+        null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.yamlfmt.with({
+        null_ls.builtins.formatting.trim_whitespace,
+        null_ls.builtins.formatting.yamlfmt.with {
           extra_args = {
-            "-formatter",
-            "retain_line_breaks=true",
-          }
-        }),
+            '-formatter',
+            'retain_line_breaks=true',
+          },
+        },
         null_ls.builtins.formatting.yapf,
       },
     }
